6775341a5243bda99638020093cc4049ebaab67c
kishorikumbhar06 <kishorikumbhar06@gmail.com>
2025-12-16 11:25:07 +0530

    Assign2Q3

---
+++ Assignment 2/Assign2Q3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+    uint16_t reg = 0xABCD;
+
+    
+    uint16_t lower4 = reg & 0x000F;
+
+    
+    
+    uint16_t upper4_lower_byte = (reg & 0x00F0) >> 4;
+
+    printf("Lower 4 bits: 0x%X\n", lower4);
+    printf("Upper 4 bits of lower byte: 0x%X\n", upper4_lower_byte);
+
+    return 0;
+}
